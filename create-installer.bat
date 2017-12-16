397fd8aa349c66929efde0e7f1b962bc24c80b5a
Kim Trang <kim.trang@couchbase.com>
2017-12-15 17:07:47 -0800

    syntax

--- create-installer.bat
+++ create-installer.bat
@@ -12,14 +12,14 @@ heat dir %installdir% -srd -suid -ag -sreg -ke -cg SyncGateway -dr INSTALLDIR -o
 :: Unfortunately -ag and -ke don't work together - it generates an
 :: illegal .wxs. Post-process here to provide random GUIDs for empty
 :: diretories.
-#python fix-empty-dir-guids.py Files.wxs || goto :error
+::python fix-empty-dir-guids.py Files.wxs || goto :error
 
 :: Compile .wxs files to intermediate objects, specifying version
 candle -dVersion=%version% -dProductName="%productname%" -arch x64 -ext WixUtilExtension *.wxs || goto :error
 
 :: Create RTF version of License file - our license text is close enough
 :: to Markdown to pass
-#pandoc ..\LICENSE-%edition%.txt -f markdown -t rtf -s -o License.rtf
+::pandoc ..\LICENSE-%edition%.txt -f markdown -t rtf -s -o License.rtf
 
 :: Light it up!
 light -ext WixUIExtension -ext WixUtilExtension -b %installdir% -dWixUILicenseRtf=License.rtf -o SGW.msi *.wixobj || goto :error
