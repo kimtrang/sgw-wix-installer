55e784da11f3ac0f3708f423d74c0c4036b9de0c
Kim Trang <kim.trang@couchbase.com>
2018-01-09 11:21:19 -0800

    test with version 2.0.1.731

--- create-installer.bat
+++ create-installer.bat
@@ -19,7 +19,7 @@ heat dir %installdir% -srd -suid -ag -sreg -ke -cg SyncGateway -dr INSTALLDIR -o
 
 :: Compile .wxs files to intermediate objects, specifying version
 ::candle -dVersion=%version% -dProductName="%productname%" -arch x64 -ext WixUtilExtension *.wxs || goto :error
-candle -dVersion="2.0.1.729" -dProductName="%productname%" -arch x64 -ext WixUtilExtension *.wxs || goto :error
+candle -dVersion="2.0.1.731" -dProductName="%productname%" -arch x64 -ext WixUtilExtension *.wxs || goto :error
 
 :: Create RTF version of License file - our license text is close enough
 :: to Markdown to pass
